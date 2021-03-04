fc3a671b8ce6bc911e63688c18188e2437a7a2f1
Naryu Ohga <naryuohga@gmail.com>
2021-03-04 12:47:58 +0900

    Add Tealium SDK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Tealium, TealConfig } from '@ionic-native/tealium/ngx';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  // export class AppComponent implements OnInit {
+  // constructor(private tealium: Tealium) { }
+
+  
+  // ngOnInit(): void {
+  //   let tealConfig: TealConfig = {
+  //     account: "services-naryu-ohga",
+  //     profile: "angular",
+  //     environment: "dev", // usually "dev", "qa" or "prod"
+  //     isLifecycleEnabled: "false", // pass "false" to disable lifecycle tracking
+  //     isCrashReporterEnabled: "false", // pass "true" to enable crash reporter (Android only)
+  //     instance: "shopapp" // an arbitrary instance name. use the same instance name for all subsequent API calls
+  //    }
+    
+  //   // The tealium.view function will call the tealium.track function with 'view' as first param
+  //   // Most tags support the 'view' event and many analytics tags also support the 'link' event
+  //   this.tealium.init(tealConfig).then(()=>{
+  //     this.tealium.trackView({"screen_name": "homescreen"}, "shopapp");
+  //   });
+  //   //this.tealium.track('view', {'event_name' : 'init'});
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -6,12 +6,33 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Tealium, TealConfig } from '@ionic-native/tealium/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, Tealium],
+  exports: [Tealium],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule implements OnInit {
+  // constructor() {}
+  constructor(private tealium: Tealium) { }
+
+  
+  ngOnInit(): void {
+    let tealConfig: TealConfig = {
+      account: "services-naryu-ohga",
+      profile: "angular",
+      environment: "dev", // usually "dev", "qa" or "prod"
+      isLifecycleEnabled: "false", // pass "false" to disable lifecycle tracking
+      isCrashReporterEnabled: "false", // pass "true" to enable crash reporter (Android only)
+      instance: "shopapp" // an arbitrary instance name. use the same instance name for all subsequent API calls
+     }
+    
+    this.tealium.init(tealConfig).then(()=>{
+      this.tealium.trackView({"screen_name": "homescreen"}, "shopapp");
+    });
+  }
+}
